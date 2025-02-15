158f66da9c32279ac259363201f19cdf32e37d8d
Cream325 Chi <pto5515@gmail.com>
2025-02-15 17:53:49 +0900

    1. runtimeError     - 에러 코드 열거형 변경(매크로 정의로 변경)     - 기존의 create() 함수 삭제     - printError() 추가

--- Structure_And_Algorithm_In_C/RuntimeError.c
+++ Structure_And_Algorithm_In_C/RuntimeError.c
@@ -1,9 +1,26 @@
 #include "RuntimeError.h"
 
-runtimeError_t create_runtimeError(errorCode_t code, const char* errorMessage) {
-	runtimeError_t error;
-	error.code = code;
-	error.errorMessage = errorMessage;
+#include <stdio.h>
 
-	return error;
+void runtimeError_printError(err_t errorCode) {
+	if (!errorCode)
+		printf("ErrorMessage: Ok.");
+
+	else {
+		// Null Pointer
+		if (errorCode & POINTER_IS_NULL != 0)
+			printf("ErrorMessage: Pointer is null.");
+		// Failed Allocation
+		else if (errorCode & FAILED_ALLOCATION != 0)
+			printf("ErrorMessage: Memory allocation is failed.");
+		// Failed Free
+		else if (errorCode & FAILED_FREE != 0)
+			printf("ErrorMessage: Memory free is failed.");
+		// Invalid Operation
+		else if (errorCode & INVALID_OPERATION != 0)
+			printf("ErrorMessage: Operation is invalid.");
+		// Unexcepted Error
+		else
+			printf("ErrorMessage: Unexcepted error.");
+	}
 }
--- Structure_And_Algorithm_In_C/RuntimeError.h
+++ Structure_And_Algorithm_In_C/RuntimeError.h
@@ -1,24 +1,33 @@
 #ifndef RUNTIME_ERROR_H
 #define RUNTIME_ERROR_H
 
-#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
 
-typedef enum {
-	Ok = 0,
-	NullOfHeader = 1000,
-	UnderflowedIndex = 1001,
-	OverflowedIndex = 1002,
-	EmptyOfStack = 2001,
-	FullOfStack = 2002,
-	EmptyOfQueue = 2003,
-	FullOfQueue = 2004
-} errorCode_t;
+// 에러 코드 목록
 
-typedef struct {
-	errorCode_t code;
-	const char* errorMessage;
-} runtimeError_t;
+// 정상 동작
+#define OK					0b0
+// 포인터가 없음
+#define POINTER_IS_NULL		0b1
+// 할당 실패
+#define FAILED_ALLOCATION	0b10
+// 해제 실패
+#define FAILED_FREE			0b100
+// 잘못된 동작
+#define INVALID_OPERATION	0b1000
+// 알 수 없는 오류
+#define UNEXCEPTED_ERROR	0b10000000
 
-runtimeError_t create_runtimeError(errorCode_t code, const char* errorMessage);
+/// <summary>
+/// 에러 타입
+/// </summary>
+typedef uint8_t err_t;
+
+/// <summary>
+/// 에러 메시지 출력
+/// </summary>
+/// <param name="errorCode">에러 코드</param>
+void runtimeError_printError(err_t errorCode);
 
 #endif
